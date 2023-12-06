5f4ca870596113283c56c8586bb0a82beb2fc079
SantiagoEvangelista <santiago.evangelista@epfl.ch>
2023-12-06 22:18:27 +0100

    aggiuntoKalman nuvo

--- src/kalmanOld.py
+++ src/kalmanOld.py
@@ -16,81 +16,51 @@ def inverseSpeedConversion(vR, vL, R, L, Cr, Cl):
     omega = (R/L)*(vR/Cr - vL/Cl)
     return v, omega
 
-def speedConverison(v,omega,R,L,Cr,Cl):
-    v_r = ((2*v + omega*L)/(2*R))*Cr
-    v_l = ((2*v - omega*L)/(2*R))*Cl
-    return v_r, v_l
-
 
 R = 20 #mm
 L = 105 #mm
-Cr = 69.33821285
-Cl = 70.74580573
-#varv = 0.0030079585145924904
-#varomega = 1.807798653077301e-05
-varv = 2.44539
-varomega = 0.0011476
+Cl = 69.33821285
+Cr = 70.74580573
  
 # State matrix A
 
-A_k_minus_1 = np.array([[1.0,0,0,0,0],[0,1.0,0,0,0],[0,0,1.0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+A_k_minus_1 = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0]])
  
 # Input matrix B
 def getB(yaw, deltak):
-    B = np.array([ [np.cos(yaw)*deltak, 0],[np.sin(yaw)*deltak, 0],[0, deltak],[1, 0],[0, 1]])
+    B = np.array([  [np.cos(yaw)*deltak, 0],[np.sin(yaw)*deltak, 0],[0, deltak]])
     return B
 
-#Jacobian of A
-def getJacobainA(yaw, deltak, v):
-    jacobianA = np.array([
-        [1.0, 0.0, -deltak * v * np.sin(yaw), deltak * np.cos(yaw), 0.0],
-        [0.0, 1.0, deltak * v * np.cos(yaw), deltak * np.sin(yaw), 0.0],
-        [0.0, 0.0, 1.0, 0.0, deltak],
-        [0.0, 0.0, 0.0, 1.0, 0.0],
-        [0.0, 0.0, 0.0, 0.0, 1.0]
-        ])
-
-    return jacobianA
-
-
 # Process noise w
-process_noise_w_k_minus_1 = np.array([0,0,0,0,0])
+process_noise_w_k_minus_1 = np.array([0,0,0])
      
 # State model noise covariance matrix Q_k
-qx = 10 
-qy = 10 
-qgamma = 0.17454 
-qv = varv
-qomega = varomega                
-Q_k = np.array([[qx, 0, 0,0,0],
-                [0, qy, 0, 0, 0],
-                [0, 0, qgamma, 0, 0],
-                [0, 0, 0, qv, 0],
-                [0, 0, 0, 0, qomega]])                 
-
+q11 = 2 
+q22 = 2 
+qtheta = 0.07                   
+Q_k = np.array([[q11, 0, 0],
+                [0, q22, 0],
+                [0, 0, qtheta]])                 
 # Measurement matrix H_k
-H_k = np.array([[1.0,0,0,0,0],[0,1.0,0,0,0],[0,0,1.0,0,0],[0,0,0,1.0,0],[0,0,0,0,1.0]])
+H_k = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0]])
                       
 # Sensor measurement noise covariance matrix R_k
-r11 = 2
-r22 = 2
-r33 = 0.0436 
-r44 = varv
-r55 = varomega
-R_k = np.array([[r11,0,0,0,0],[0,r22,0,0,0],[0,0,r33,0,0],[0,0,0,r44,0],[0,0,0,0,r55]])
+r11 = 0.7 
+r22 = 0.7 
+r33 = 0.0014 
+R_k = np.array([[r11,0,0],[0,r22,0],[0,0,r33]])
 
 # Sensor measurement noise covariance matrix R_k_nc
 #R_k_nc = np.array([[np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf]])
-R_k_nc = np.array([[np.inf,0,0,0,0],[0,np.inf,0,0,0],[0,0,np.inf,0,0],[0,0,0,r44,0],[0,0,0,0,r55]])
+R_k_nc = np.array([[np.inf,0,0],[0,np.inf,0],[0,0,np.inf]])
 
                  
 # Sensor noise v
-sensor_noise_v_k = np.array([0,0,0,0,0])
+sensor_noise_v_k = np.array([0,0,0])
 
 # Initial state covariance matrix
 # choose how to initialize it
-P_k_minus_1 = np.array([[r11,0,0,0,0],[0,r22,0,0,0],[0,0,r33,0,0],[0,0,0,r44,0],[0,0,0,0,r55]])
-
+P_k_minus_1 = np.array([[r11,0,0],[0,r22,0],[0,0,r33]]) 
                                                
                                                
 
@@ -106,8 +76,7 @@ def ekf(z_k_observation_vector, state_estimate_k_minus_1,control_vector_k_minus_
     #print(state_estimate_k)             
     # Predicton the state covariance estimate 
     yaw = state_estimate_k[2]
-    v = control_vector_k_minus_1[0]
-    P_k = getJacobainA(yaw, dk, v) @ P_k_minus_1 @ getJacobainA(yaw, dk, v).T + Q_k
+    P_k = A_k_minus_1 @ P_k_minus_1 @ A_k_minus_1.T + Q_k
          
     # Update step
 
@@ -134,14 +103,11 @@ def ekf(z_k_observation_vector, state_estimate_k_minus_1,control_vector_k_minus_
 
 
 
-
-def estimatePosition(frame,previousControlVector,dt,P_k_minus_1,estimatedState_1,measLS,measRS):
+def estimatePosition(frame,previousControlVector,dt,P_k_minus_1,estimatedState_1):
 
     globPosition, globAngle, __ = findThymio(frame)
-    v,w = inverseSpeedConversion(measRS,measLS,R,L,Cr,Cl)
 
-    z_k_observation_vector = np.array([globPosition[0],IMAGE_HEIGHT-globPosition[1],globAngle,v,w])
-    print(z_k_observation_vector)
+    z_k_observation_vector = np.array([globPosition[0],IMAGE_HEIGHT-globPosition[1],globAngle])
 
     if globPosition[0] == -1:
         try:    
@@ -157,3 +123,4 @@ def estimatePosition(frame,previousControlVector,dt,P_k_minus_1,estimatedState_1
     angle = estimatedState[2]
 
     return position, angle, estimatedState, P_k
+
